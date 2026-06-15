3e8bcc1f305fa72d0e60b6c2aaf9d91a8a940c8b
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-06-15 16:04:41 +0900

    feat : 프로젝트 찾기 & 포트폴리오 찾기 최신 개수 제한

--- backend/src/main/java/com/backend/common/domain/portfolio/portfolio/repository/PortfolioRepository.java
+++ backend/src/main/java/com/backend/common/domain/portfolio/portfolio/repository/PortfolioRepository.java
@@ -2,10 +2,20 @@ package com.backend.common.domain.portfolio.portfolio.repository;
 
 import com.backend.common.domain.portfolio.portfolio.entity.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 
     Optional<Portfolio> findByMemberId(Long memberId);
+
+    @Query("""
+            SELECT p
+            FROM Portfolio p
+            WHERE p.isPublished = true
+            ORDER BY p.createdAt DESC
+            """)
+    List<Portfolio> findLatestPublished();
 }
